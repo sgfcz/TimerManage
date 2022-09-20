ceaf56f447a6f4599c2b1e9a0aedd6c758520ae8
sgfcz <coderxiao@sohu.com>
2022-09-20 17:03:48 +0800

    优化using

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,23 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Collections.ObjectModel;
 using TimeManager.View;
 using TimeManager.Core;
+using TimeManager.Model;
+using System.Windows.Controls;
 
 namespace TimeManager
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
