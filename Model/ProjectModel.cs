7076d876f6c39c18a4742c0627df089fd2812a13
sgfcz <coderxiao@sohu.com>
2022-09-20 16:57:44 +0800

    1增加绑定数据类和绑定数据的委托事件 2.增加读取数据的model

--- Model/ProjectModel.cs
+++ Model/ProjectModel.cs
@@ -1,13 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Collections.ObjectModel;
 
 namespace TimeManager.Model
 {
-    internal class ProjectModel
+    public delegate void MainCreateSuccess(string name);
+    public delegate void MainDeleteSuccess(object item);
+
+    public class ProjectNames : INotifyPropertyChanged
     {
-        
+        private string? name;
+        public string? Name
+        {
+            get { return this.name; }
+            set
+            {
+                this.name = value;
+                this.NotifyPropertyChanged("Name");
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void NotifyPropertyChanged(string projectName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(projectName));
+        }
+    }
+
+    public class ProjectMessages
+    {
+        private string counttime;
+        public string CountTime { get { return this.counttime; } set { counttime = value; } }
+        private int times;
+        public int Times { get { return this.times; } set { times = value; } }
     }
 }
